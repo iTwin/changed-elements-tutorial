650a9ffd5a9a18be22a4bef12477a61c28787a86
Diego Pinate <diego.pinate@bentley.com>
2021-06-15 15:40:59 -0400

    Add changed elements UI provider and add it to the app

---
+++ src/ChangedElementsUiProvider.tsx
@@ -0,0 +1,45 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import {
+  AbstractWidgetProps,
+  StagePanelLocation,
+  StagePanelSection,
+  UiItemsProvider,
+} from '@bentley/ui-abstract';
+import { UiFramework } from '@bentley/ui-framework';
+import * as React from 'react';
+import { ChangedElementsWidget } from './ChangedElementsWidget';
+
+export class ChangedElementsUiProvider implements UiItemsProvider {
+  public readonly id = 'ChangedElementsProviderId';
+  
+  public provideWidgets(
+    stageId: string,
+    stageUsage: string,
+    location: StagePanelLocation,
+    section?: StagePanelSection
+  ): ReadonlyArray<AbstractWidgetProps> {
+
+    const widgets: AbstractWidgetProps[] = [];
+    if (
+      location === StagePanelLocation.Right &&
+      section === StagePanelSection.Start
+    ) {
+      const changedElementsWidget: AbstractWidgetProps = {
+        id: 'ChangedElementsWidget',
+        label: 'Changed Elements',
+        getWidgetContent() {
+          return (
+            <ChangedElementsWidget iModel={UiFramework.getIModelConnection()} />
+          );
+        },
+      };
+
+      widgets.push(changedElementsWidget);
+    }
+
+    return widgets;
+  }
+}
